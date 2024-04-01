778b78b5d8e57b538f6f00a312764c8691dacf52
svr2603 <latessad@mail.ru>
2024-04-01 23:30:40 +0300

    add new code

--- main.py
+++ main.py
@@ -24,6 +24,60 @@ class Animal():
         pass
 
 class Bird(Animal):
-    def __init__(self, name, age):
+    def __init__(self, name, age, color):
         super().__init__(name, age)
-        
+        self.color = color
+    def fly(self):
+        print(f"{self.name} летает")
+    def make_sound(self):
+        print(f"{self.name} щебечет")
+
+class Mammal(Animal):
+    def __init__(self, name, age, predator):
+        super().__init__(name, age)
+        self.predator = predator
+
+    def hunting(self):
+        print(f"{self.name} охотится")
+    def make_sound(self):
+        print(f"{self.name} рычит")
+class Reptile(Animal):
+    def __init__(self, name, age, venomous):
+        super().__init__(name, age)
+        self.venomous = venomous
+
+    def crawling(self):
+        print(f"{self.name} ползает")
+
+    def make_sound(self):
+        print(f'{self.name} шипит')
+
+def animal_sound(animals):
+    for animal in animals:
+        animal.make_sound()
+
+
+class Zookeeper():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def feed_animal(self):
+        print(f"{self.name} кормит животных")
+
+class Veterinarian():
+    def __init__(self, name, age, specialization):
+        self.name = name
+        self.age = age
+        self.specialization = specialization
+    def heal_animal(self):
+        print(f"{self.name} лечит животных")
+class Zoo():
+    def __init__(self):
+        self.animals = []
+        self.stuff = []
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+
+    def add_stuff(self, stuff):
+        self.stuff.append(stuff)
